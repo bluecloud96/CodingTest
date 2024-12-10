68772851cbcbc3d04085ce9489fc81cf00e9e866
bluecloud96 <166796528+bluecloud96@users.noreply.github.com>
2024-12-11 00:30:41 +0900

    [level 1] Title: 특정 형질을 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
+++ 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
@@ -1,8 +1,8 @@
-
-SELECT
+SELECT 
     COUNT(*) AS COUNT
 FROM
     ECOLI_DATA
 WHERE
-    (GENOTYPE & 2 = 0)
-    AND (GENOTYPE & 1 > 0 OR GENOTYPE & 4 > 0);
+    (GENOTYPE & 2) = 0
+AND
+    ((GENOTYPE & 1) > 0 OR (GENOTYPE & 4) > 0);
